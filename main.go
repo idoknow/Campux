c0554154e3489bca20f444f3e59cab11c6d01bb1
RockChinQ <1010553892@qq.com>
2024-04-11 10:11:53 +0000

    feat: 文件存到 data 目录

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"github.com/RockChinQ/Campux/backend/config"
 	"github.com/RockChinQ/Campux/backend/core"
+	"github.com/RockChinQ/Campux/backend/util"
 )
 
 func main() {
+
+	// 检查目录data
+	err := util.MakeSureDirExist("data")
+
+	if err != nil {
+		panic(err)
+	}
+
 	// 配置文件
 	_, created, err := config.NewConfig()
 
