6026d0177ac053ad683be714e0f3c2ee6db95f14
RockChinQ <1010553892@qq.com>
2024-04-26 05:01:12 +0000

    feat: 通过redis stream传递稿件到bot

--- backend/core/app.go
+++ backend/core/app.go
@@ -1,10 +1,15 @@
 package core
 
 import (
+	"time"
+
 	"github.com/RockChinQ/Campux/backend/controller"
 	"github.com/RockChinQ/Campux/backend/database"
+	"github.com/RockChinQ/Campux/backend/mq"
 	"github.com/RockChinQ/Campux/backend/oss"
 	"github.com/RockChinQ/Campux/backend/service"
+	"github.com/RockChinQ/Campux/backend/service/routine"
+	gocron "github.com/go-co-op/gocron/v2"
 )
 
 type Application struct {
@@ -14,16 +19,49 @@ type Application struct {
 func NewApplication() *Application {
 
 	db := database.NewMongoDBManager()
-	as := service.NewAccountService(*db)
-
 	fs := oss.NewMinioClient()
-	ps := service.NewPostService(*db, *fs)
+	msq := mq.NewRedisStreamMQ()
 
+	as := service.NewAccountService(*db)
+	ps := service.NewPostService(*db, *fs)
 	ms := service.NewMiscService(*db)
+
+	err := ScheduleRoutines(*db, *msq)
+	if err != nil {
+		panic(err)
+	}
 
 	return &Application{
 		API: controller.NewApiController(*as, *ps, *ms),
 	}
+}
+
+func ScheduleRoutines(
+	db database.MongoDBManager,
+	msq mq.RedisStreamMQ,
+) error {
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		return err
+	}
+
+	_, err = s.NewJob(
+		gocron.DurationJob(
+			20*time.Second,
+		),
+		gocron.NewTask(
+			routine.SchedulePublishing,
+			db,
+			msq,
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	s.Start()
+
+	return nil
 }
 
 func (a *Application) Run() {
