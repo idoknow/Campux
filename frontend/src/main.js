c71e557b74e082383fb3dfa8227343db4365ad07
Soulter <905617992@qq.com>
2024-04-28 22:49:28 -0400

    fix: baseurl

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,16 +16,14 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import store from './store/index.js'
 import { ref } from 'vue'
-import { useStore } from "vuex"
 
 const app = createApp(App)
 app.use(store)
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: 'https://dev.campux.idoknow.top'
+    baseURL: store.state.base_url,
 })
-app.config.globalProperties.$baseurl = ref('https://dev.campux.idoknow.top')
 app.config.globalProperties.$axios = { ...axiosInstance }
 
 registerPlugins(app)
