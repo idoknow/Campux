a3b98bf44b484b5b47870ee8ec9b758038887f8a
RockChinQ <1010553892@qq.com>
2024-05-10 22:04:26 +0800

    fix: cookies无法由js修改问题

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -23,17 +23,22 @@ app.use(store)
 app.use(VueCookies)
 
 // let config=require("../config.json");
-fetch('/config.json').then(response => response.json()).then(config => {
-    console.log(config)
-    store.commit('setBaseURL', config.base_url)
-}).then(() => {
-    const axiosInstance = axios.create({
-        withCredentials: true,
-        baseURL: store.state.base_url,
-    })
-    app.config.globalProperties.$axios = { ...axiosInstance }
-})
+// fetch('/config.json').then(response => response.json()).then(config => {
+//     console.log(config)
+//     store.commit('setBaseURL', config.base_url)
+// }).then(() => {
+//     const axiosInstance = axios.create({
+//         withCredentials: true,
+//         baseURL: store.state.base_url,
+//     })
+//     app.config.globalProperties.$axios = { ...axiosInstance }
+// })
 
+const axiosInstance = axios.create({
+    withCredentials: true,
+    baseURL: store.state.base_url,
+})
+app.config.globalProperties.$axios = { ...axiosInstance }
 
 registerPlugins(app)
 
