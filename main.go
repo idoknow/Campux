70948c66b437f0558b2260e3ccfb6405b92d916e
RockChinQ <1010553892@qq.com>
2024-08-26 18:30:46 +0800

    feat: 支持使用本地存储

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/RockChinQ/Campux/backend/config"
 	"github.com/RockChinQ/Campux/backend/core"
+	"github.com/RockChinQ/Campux/backend/migrate"
 	"github.com/RockChinQ/Campux/backend/util"
 )
 
@@ -24,6 +25,12 @@ func main() {
 
 	if created {
 		panic("请修改配置文件后重启")
+	}
+
+	err = migrate.DoMigration()
+
+	if err != nil {
+		panic(err)
 	}
 
 	// 启动服务
